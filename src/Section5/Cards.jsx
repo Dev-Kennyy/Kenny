ac547f066222aa96adb00130ccded1e79e769b4c
Dev-Kennyy <kehindesalimonu1@gmail.com>
2025-08-04 22:48:30 +0100

    Fixed project detailss

--- src/Section5/Cards.jsx
+++ src/Section5/Cards.jsx
@@ -5,7 +5,7 @@ function Cards() {
     <div className="flex flex-wrap justify-center gap-5 px-4 pt-8 sm:flex-nowrap">
       <Card
         header="E-Commerce"
-        client="Dev_Kenny"
+        client="Web_Clone"
         details="A clone of my university’s official landing page designed to replicate the structure, layout, & design of the original site. Built to showcase front-end skills in translating real-world designs into responsive web pages"
         src="Screenshot (274).png"
         height="h-[330px]"
@@ -13,7 +13,7 @@ function Cards() {
       />
       <Card
         header="E-Commerce"
-        client="John Ajala"
+        client="Pizza App"
         details="A responsive web application that allows users to order pizza online with a smooth and intuitive user interface. Users can browse a selection of pizzas, customize their orders (e.g., size, quantity, toppings), and add items to their cart."
         src="Screenshot (273).png"
         height="h-[361px]"
@@ -21,7 +21,7 @@ function Cards() {
       />
       <Card
         header="E-Commerce"
-        client="Dev_Kenny"
+        client="Store"
         details="A fully responsive E-Commerce website built to simulate a modern online shopping experience. Users can browse products, add items, perform authentication, order product. Designed with a user-centric UI and optimized performance with a touch of framer motion for animating scroll"
         src="Screenshot (272).png"
         height="h-[390px]"
