de26edf5be58f938a2d241ed8789c084c1f0a844
Dev-Kennyy <kehindesalimonu1@gmail.com>
2025-11-04 15:37:01 +0100

    KKS

--- src/Section6/Cards.jsx
+++ src/Section6/Cards.jsx
@@ -13,13 +13,13 @@ function Cards() {
           />
           <Card
             img="anonymous.jpeg"
-            p1="Collaborator"
-            h="Hospital"
-            p2="Blessing is very creative and always brings fresh ideas to projects. Working with him was smooth and enjoyable."
+            p1="Intern"
+            h="BUTH Hospital"
+            p2="Blessing is very creative and always brings fresh ideas to projects. Working with him was smooth and enjoyable. A good team player indeed"
           />
           <Card
             img="anonymous.jpeg"
-            p1="Freelance Client"
+            p1="Frontend Contract"
             h="Eden Estates"
             p2="Blessing is highly skilled, professional, and reliable. He delivered a clean, well-structured codebase for our project. I’d definitely recommend and work with him again."
           />
